198e83b66aefd21366408a7a3ffc0ce3f6d5b7e8
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-12-27 22:00:13 +0100

    feat: initialize project

---
+++ src/Button.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Button } from './Button'
+
+const meta = {
+  title: 'Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof Button>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const MyButton: Story = {
+  args: {},
+}
---
+++ src/Button.tsx
@@ -0,0 +1,5 @@
+import type { ButtonProps } from './Button.type'
+
+export function Button(props: ButtonProps) {
+  return <button {...props} />
+}
